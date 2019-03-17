bf414e175927a1eff2eb7290cffd41aaaa474a56
Horneac Emanuel <horneac.emanuel@yahoo.com>
2019-03-17 18:53:50 +0200

    change schema name from 'stack_overflow_test' to 'stack_overflow'

--- assignment1/src/main/resources/db/migration/V1__Initial_schema.sql
+++ assignment1/src/main/resources/db/migration/V1__Initial_schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `stack_overflow_test`.`tag` (
+CREATE TABLE `stack_overflow`.`tag` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC));
 
-  CREATE TABLE `stack_overflow_test`.`user` (
+  CREATE TABLE `stack_overflow`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `stack_overflow_test`.`tag` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC));
 
-CREATE TABLE `stack_overflow_test`.`question` (
+CREATE TABLE `stack_overflow`.`question` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `author` VARCHAR(45) NOT NULL,
   `title` VARCHAR(45) NOT NULL,
@@ -22,23 +22,23 @@ CREATE TABLE `stack_overflow_test`.`question` (
   `creation_date_time` DATETIME NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `stack_overflow_test`.`question_tag` (
+CREATE TABLE `stack_overflow`.`question_tag` (
   `questionID` INT NOT NULL,
   `tagID` INT NOT NULL,
   PRIMARY KEY (`questionID`, `tagID`),
   INDEX `tag_question_idx` (`tagID` ASC),
   CONSTRAINT `question_tag`
     FOREIGN KEY (`questionID`)
-    REFERENCES `stack_overflow_test`.`question` (`id`)
+    REFERENCES `stack_overflow`.`question` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `tag_question`
     FOREIGN KEY (`tagID`)
-    REFERENCES `stack_overflow_test`.`tag` (`id`)
+    REFERENCES `stack_overflow`.`tag` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT);
 
-CREATE TABLE `stack_overflow_test`.`answer` (
+CREATE TABLE `stack_overflow`.`answer` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `author` VARCHAR(45) NOT NULL,
   `text` VARCHAR(45) NOT NULL,
@@ -48,11 +48,11 @@ CREATE TABLE `stack_overflow_test`.`answer` (
   INDEX `answer_question_idx` (`questionID` ASC),
   CONSTRAINT `answer_question`
     FOREIGN KEY (`questionID`)
-    REFERENCES `stack_overflow_test`.`question` (`id`)
+    REFERENCES `stack_overflow`.`question` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT);
 
-CREATE TABLE `stack_overflow_test`.`vote` (
+CREATE TABLE `stack_overflow`.`vote` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `type` INT NOT NULL,
   `userID` INT NOT NULL,
@@ -64,16 +64,16 @@ CREATE TABLE `stack_overflow_test`.`vote` (
   INDEX `vote_answer_idx` (`answerID` ASC),
   CONSTRAINT `user_vote`
     FOREIGN KEY (`userID`)
-    REFERENCES `stack_overflow_test`.`user` (`id`)
+    REFERENCES `stack_overflow`.`user` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `vote_question`
     FOREIGN KEY (`questionID`)
-    REFERENCES `stack_overflow_test`.`question` (`id`)
+    REFERENCES `stack_overflow`.`question` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `vote_answer`
     FOREIGN KEY (`answerID`)
-    REFERENCES `stack_overflow_test`.`answer` (`id`)
+    REFERENCES `stack_overflow`.`answer` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT);
