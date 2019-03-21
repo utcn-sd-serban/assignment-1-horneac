d2972e3111634a26a5b99e0725ca37fcc6328a20
Horneac Emanuel <horneac.emanuel@yahoo.com>
2019-03-21 22:49:17 +0200

    added service class for questions

---
+++ assignment1/src/main/java/ro/utcn/sd/he/assignment1/service/QuestionService.java
@@ -0,0 +1,29 @@
+package ro.utcn.sd.he.assignment1.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ro.utcn.sd.he.assignment1.model.Question;
+import ro.utcn.sd.he.assignment1.persistence.api.RepositoryFactory;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final RepositoryFactory factory ;
+
+    @Transactional
+    public List<Question> listQuestions(){return factory.createQuestionRepository().findAll();}
+
+    @Transactional
+    public Question saveQuestion(Question question){
+        return factory.createQuestionRepository().save(question);
+    }
+
+    public void deleteQuestion(int id){
+        Optional<Question> question = factory.createQuestionRepository().findById(id);
+        factory.createQuestionRepository().remove(question.get());
+    }
+}
